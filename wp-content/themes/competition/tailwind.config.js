a91d86ad992c1611564b739e6825e74acbfe43c9
jayeshw3nuts <86507758+jayeshw3nuts@users.noreply.github.com>
2021-06-26 17:59:49 +0530

    plugin added

--- wp-content/themes/competition/tailwind.config.js
+++ wp-content/themes/competition/tailwind.config.js
@@ -1,14 +1,18 @@
 module.exports = {
-  purge: [
-    './src/**/*.html',
-    './src/**/*.js',
-  ],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    purge: [
+        './src/**/*.html',
+        './src/**/*.js',
+    ],
+    darkMode: false, // or 'media' or 'class'
+        theme: {
+        extend: {
+            fontFamily: {
+                'sans': ['Roboto', 'Helvetica', 'Arial', 'sans-serif']
+            }
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 }
